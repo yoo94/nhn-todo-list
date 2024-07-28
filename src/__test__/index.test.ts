80772142960b649f1cf22a1b58bea1a506e6377d
yoo94 <ahy0119@naver.com>
2024-07-28 11:23:34 +0900

    test: 테스트 대상 변수명 변경

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -23,35 +23,38 @@ describe('TodoApp', () => {
         todoApp.createTodo('old Test Todo');
         todoApp.createTodo('new Test Todo');
 
-        expect(todoApp.todos.length).toBe(2);
-        expect(todoApp.todos[0].text).toBe('new Test Todo');
-        expect(todoApp.todos[1].text).toBe('old Test Todo')
+        expect(todoApp.allTodos.length).toBe(2);
+        expect(todoApp.allTodos[0].text).toBe('new Test Todo');
+        expect(todoApp.allTodos[1].text).toBe('old Test Todo')
     });
 
     test('todo 아이템 삭제 테스트', () => {
         todoApp.createTodo('old Test Todo');
         todoApp.createTodo('new Test Todo');
 
-        expect(todoApp.todos.length).toBe(2);
+        expect(todoApp.allTodos.length).toBe(2);
 
-        const todoToDelete = todoApp.todos[1].id;
+        const todoToDelete = todoApp.allTodos[1].id;
         todoApp.deleteTodo(todoToDelete);
 
-        expect(todoApp.todos.length).toBe(1);
-        expect(todoApp.todos[0].text).toBe('new Test Todo');
+        expect(todoApp.allTodos.length).toBe(1);
+        expect(todoApp.allTodos[0].text).toBe('new Test Todo');
     });
 
     test('todo 아이템 토글 상태변경 테스트', () => {
         // 상태변경에 대한 반복적인 테스트 구현
         todoApp.createTodo('Test Todo');
 
-        const todoItem = todoApp.todos[0];
+        const todoItem = todoApp.allTodos[0];
+
         expect(todoItem.completed).toBe(false);
 
         todoItem.toggleCompletion();
+
         expect(todoItem.completed).toBe(true);
 
         todoItem.toggleCompletion();
+
         expect(todoItem.completed).toBe(false);
     });
 });
